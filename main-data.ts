bad2a83c657e0e67dcf2175a112eb8cbb6a543da
Joao Medeiros <corona>
2020-10-16 15:43:05 -0300

    setup create-react-app

---
+++ main-data.ts
@@ -0,0 +1,16 @@
+const thingsToBuy = [
+  {
+    name: "mattress",
+  },
+  {
+    name: "chair",
+  },
+  {
+    name: "t-shirts",
+  },
+  {
+    name: "earbuds",
+  },
+];
+
+const glassesOfWater = {};
